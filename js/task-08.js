abb65559f2fc151e5694d2e5e27e233937783180
Sergej Tarasenko <sergej.tarasenko@murka.com>
2023-10-13 20:18:07 +0300

    [+] task 03

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", onLoginFormSubmit);
 
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All fields must be filled!");
+    return;
+  }
+
+  const loginData = {
+    email,
+    password,
+  };
+
+  console.log(loginData);
+
+  event.currentTarget.reset();
+}
